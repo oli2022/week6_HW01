b9b6f339cfa1c7a0c43849b8b494cf9a08921aab
Oli <oli0970717429@gmail.com>
2022-06-06 13:21:50 +0800

    feat:新增檔案

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const router = express.Router();
+
+// 引用 userContr.js檔案
+const userContr = require('../controllers/userContr');
+
+/**
+ * 注意‼️
+ * 路徑還沒有改好
+ */
+router.get('/posts', (req, res, next) => userContr.getAllPosts(req, res, next)); // 註冊
+router.post('/post', (req, res, next) => userContr.createPost(req, res, next)); // 登入
+router.delete('/post/:id', (req, res, next) => userContr.deleteOne(req, res, next)); // 重設密碼
+router.delete('/posts', (req, res, next) => userContr.deleteAll(req, res, next)); // 取得個人資料
+router.patch('/post/:id', (req, res, next) => userContr.updatePost(req, res, next)); // 更新個人資料
 
 module.exports = router;
