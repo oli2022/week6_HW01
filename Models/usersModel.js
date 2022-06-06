d64f6bcdc00a6ff6fb1619d4d238a583da9e3049
Oli <oli0970717429@gmail.com>
2022-06-06 13:20:46 +0800

    feat:新增user 欄位

--- Models/usersModel.js
+++ Models/usersModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: {
-        type: 'string',
+        type: String,
         required: [true, '請輸入您的名字'],
     },
     email: {
@@ -13,6 +13,21 @@ const userSchema = new mongoose.Schema({
         select: false,
     },
     photo: String,
+    sex: {
+        type: String,
+        enum: ['male', 'female'],
+    },
+    password: {
+        type: String,
+        required: [true, '請輸入密碼'],
+        minlength: 8,
+        select: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        select: false,
+    },
 });
 
 const User = mongoose.model('user', userSchema);
